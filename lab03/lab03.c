bd5b4ebcb7be3dcb1d208f59e5c518f721d9be0a
iamchristiancollins <105734903+iamchristiancollins@users.noreply.github.com>
2024-01-24 14:47:24 +0000

    Add comments and function

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
+void printUserInput();
+
 int main(){
 
+    printUserInput();
+
+
+    return 0;
+}
+/*
+Description: This functions prompts the user for two integers and prints the resulting sum
+Argument: None
+Returns: None
+*/
+void printUserInput(){
     int a, b, c;
     printf("Enter an integer: ");
-    scanf("%d", &a);
+    scanf("%d", &a);    // stores number that user types in a
     printf("Enter a second integer: ");
-    scanf("%d", &b);
+    scanf("%d", &b);    // stores number user types in b
 
     c = a + b;
 
     printf("The sum of %d and %d is %d\n", a, b, c);
-
-    return 0;
 }
