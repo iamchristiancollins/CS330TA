1c4a87a97ed34dd517f1be1918f536f7e75c2e39
iamchristiancollins <105734903+iamchristiancollins@users.noreply.github.com>
2024-01-24 02:19:54 +0000

    create example

---
+++ lab03/lab03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int a, b, c;
+    printf("Enter an integer: ");
+    scanf("%d", &a);
+    printf("Enter a second integer: ");
+    scanf("%d", &b);
+
+    c = a + b;
+
+    printf("The sum of %d and %d is %d\n", a, b, c);
+
+    return 0;
+}
